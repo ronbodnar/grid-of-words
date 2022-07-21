2c1d9001cd14db6ddd0af0bd06c23193a0e50b20
Ron Bodnar <ron.bodnar@outlook.com>
2022-07-21 02:16:25 -0700

    Moved the active class from individual squares to the whole row, to enable a loading animation and letter validation reveal animations later on.

--- src/public/assets/js/components/board/row.js
+++ src/public/assets/js/components/board/row.js
@@ -1,5 +1,6 @@
 import { generatedSquare, updateSquareBackground } from "./square.js";
 import { getValidatedLetters } from "../../utils/helpers.js";
+import { getLoader } from "../loader.js";
 
 /*
  * Generates a row of squares of the word length for the game.
@@ -8,15 +9,21 @@ import { getValidatedLetters } from "../../utils/helpers.js";
  * @returns {HTMLDivElement} - The generated row with all squares.
  */
 function generatedRow(index, cols, game) {
+  const active = index === (game?.attempts?.length || 0);
   const row = document.createElement("div");
   row.classList.add("word-row");
+  if (active) row.classList.add("active");
+
+  const loader = getLoader();
+  loader.classList.add("hidden");
+
+  row.appendChild(loader);
 
   // Add squares equal to the length of the word in the row
   for (var j = 0; j < cols; j++) {
-    const active = index === (game?.attempts?.length || 0);
-    const square = generatedSquare(active);
+    const square = generatedSquare();
 
-    // Update the square background if the letter is not in the word
+    // Update the square background if the letter is not in the previous attempt(s).
     if (game && index < game.attempts.length) {
       square.children[0].textContent = game.attempts[index].at(j).toUpperCase();
 
